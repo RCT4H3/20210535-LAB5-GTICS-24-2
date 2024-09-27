f685d70274133b00ee7129f6a71e530da005f92d
RCT4H3 <139926077+RCT4H3@users.noreply.github.com>
2024-09-26 19:28:24 -0500

    Creación del proyecto

---
+++ LAB3_20210535/src/test/java/com/example/lab3_20210535/Lab320210535ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20210535;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab320210535ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
